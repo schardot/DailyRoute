b9ae875e4e328a3c551044c63599078739551015
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-01-27 15:35:16 +0100

    Fix initial goal state and clean up goal setting logic in player script

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var  speed := 200.0
 var goal_shape: GameTypes.ShapeType
 var goal_color: GameTypes.ColorType
-var has_goal := true
+var has_goal := false
 
 @onready var thought_bubble := $ThoughtBubble
 
@@ -30,5 +30,11 @@ func _ready() -> void:
 		GameTypes.ShapeType.TRIANGLE,
 		GameTypes.ColorType.BLUE
 	)
-	#goal_shape = GameTypes.ShapeType.CIRCLE
-	#goal_color = GameTypes.ColorType.RED
+	
+
+func set_goal(shape: GameTypes.ShapeType, color: GameTypes.ColorType):
+	goal_shape = shape
+	goal_color = color
+	has_goal = true
+
+	thought_bubble.set_icon(shape, color)
