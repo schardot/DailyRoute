7ef85585e72bc8d0642f7099b779323d9b503a48
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-01-27 15:35:20 +0100

    Remove unused icon display script from player scene

--- scenes/player/icon_display.gd
+++
@@ -1,8 +0,0 @@
-@export var shape: GameTypes.ShapeType
-@export var color: GameTypes.ColorType
-
-@onready var icon_label := $Label
-@onready var icon_rect := $ColorRect
-
-func update_icon():
-	# same code as stores
