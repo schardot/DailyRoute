a96ce21ac0d4949a1f771326a02a40aecb2d698d
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-03-12 16:36:27 +0100

    refactor(tutorial): implemented init functions on ready to increase readability

--- systems/TutorialController.gd
+++ systems/TutorialController.gd
@@ -12,32 +12,17 @@ var store_map := {}
 func _ready() -> void:
 	await get_tree().process_frame
 	
-	stores = get_tree().get_nodes_in_group("stores")
-	assert(stores.size() > 0)
-	
-	for store in stores:
-		store_map[store.store_id] = store
-		store.player_entered.connect(on_assignment_completed)
+	init_stores()
 	
 	generate_assignment()
 
 func generate_assignment():
-	
 	var currentStoreNum : int = assignment_order[current_phase]
 	var currentStoreNode : Node = store_map[currentStoreNum]
 	
 	player.set_goal(currentStoreNode.color)
 	currentStoreNode.unblock_store()
 	emit_signal("store_opened")
-	print(
-		"NEW ASSIGNMENT:",
-		"
-		Current phase: ", current_phase,
-		" 
-		Current Store Num:", currentStoreNum,
-		"
-		Current store node:", currentStoreNode
-	)
 	apply_phase_movement_rules()
 
 func on_assignment_completed() -> void:
@@ -63,10 +48,18 @@ func apply_phase_movement_rules():
 func tutorial_complete() -> void:
 	player.clear_goal()
 	reset_stores()
-	print("Tutorial completed")
 	SceneManager.player_position = player.global_position
 	SceneManager.go_to_game()
 
 func reset_stores():
 	for store in stores:
 		store.completed = false
+
+func init_stores():
+	stores = get_tree().get_nodes_in_group("stores")
+	assert(stores.size() > 0)
+	
+	for store in stores:
+		store_map[store.store_id] = store
+		store.player_entered.connect(on_assignment_completed)
+	
